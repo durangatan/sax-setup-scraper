c542fda99b12b6f90043932e866c64cc82ffdea0
durangatan <joeduran8@gmail.com>
2018-10-14 00:04:52 -0500

    adds sunderella

---
+++ sites/sunderella/index.js
@@ -0,0 +1,6 @@
+import scrapeFunction from './scrapeFunction';
+export default {
+  sourceUrl: 'http://www.sunderella.zc.bz/bbs/skin/ggambo7002_board/print.php?id=qna&no=39',
+  scrapeFunction,
+  outputFile: './sites/sunderella/setups.json'
+};
---
+++ sites/sunderella/scrapeFunction.js
@@ -0,0 +1,32 @@
+import striptags from 'striptags';
+export default function scrapeFunction(jsDom) {
+  const [names] = jsDom.getElementsByTagName('div');
+  const cleanNames = striptags(names.innerHTML)
+    .trim()
+    .split(/\n/)
+    .filter(val => val.length);
+  const playerIndicies = [];
+  cleanNames.forEach((rowValue, index) => {
+    if (rowValue === 'Instrument Mouthpiece Reed') {
+      playerIndicies.push(index - 1);
+    }
+  });
+  const players = [];
+  let currentPlayer = {};
+  cleanNames.forEach((rowValue, index) => {
+    if (playerIndicies.includes(index)) {
+      if (currentPlayer.name) {
+        players.push(currentPlayer);
+      }
+      currentPlayer = { name: rowValue, setups: [] };
+      return;
+    }
+    if (rowValue !== 'Instrument Mouthpiece Reed' && rowValue.length > 2) {
+      currentPlayer.setups.push({ misc: rowValue });
+    }
+  });
+  return {
+    players,
+    allPropertyNames: ['misc']
+  };
+}
