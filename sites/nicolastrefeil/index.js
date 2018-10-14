b07b4644e893da08bf2520f3655c6e09b5840e7a
durangatan <joeduran8@gmail.com>
2018-10-13 20:40:52 -0500

    adds scraped data for nicolas

---
+++ sites/nicolastrefeil/index.js
@@ -0,0 +1,6 @@
+import scrapeFunction from './scrapeFunction';
+export default {
+  sourceUrl: 'https://www.nicolastrefeil.com/tenor-sax-player-s-setup',
+  scrapeFunction,
+  outputFile: './sites/nicolastrefeil/setups.json'
+};
---
+++ sites/nicolastrefeil/scrapeFunction.js
@@ -0,0 +1,36 @@
+import uniq from 'lodash.uniq';
+import { getCleanData } from '../../scraper/utils';
+
+export default function scrapeFunction(jsDoc) {
+  const allPropertyNames = [];
+  const tableClass = 'webs-table';
+  const tables = jsDoc.getElementsByClassName(tableClass);
+  const players = Array.from(tables).map(table => {
+    const [tHead, ...tRows] = table.children;
+    const player = {};
+    const [name, ...propNames] = tHead.children[0].children;
+    player.name = getCleanData(name);
+    player.setups = Array.from(tRows).map(setup => {
+      const setupRow = setup.children[0];
+      const setupProps = {};
+      propNames
+        .map(propName => {
+          const cleanedPropName = getCleanData(propName).toLowerCase();
+          allPropertyNames.push(cleanedPropName);
+          return cleanedPropName;
+        })
+        .forEach((propName, index) => {
+          const cleanPropValue = getCleanData(setupRow.children[index + 1]);
+          if (cleanPropValue && cleanPropValue.length && cleanPropValue !== '.') {
+            setupProps[propName.toLowerCase()] = cleanPropValue;
+          }
+        });
+      return setupProps;
+    });
+    return player;
+  });
+  return {
+    allPropertyNames: uniq(allPropertyNames),
+    players
+  };
+}
